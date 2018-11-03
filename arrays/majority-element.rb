9a013a599aab9ec829646906a155752ad00e04b5
Vladimir Kurnavenkov <vkurnavenkov@gmail.com>
2018-11-03 01:19:27 -0700

    reorganize files

--- arrays/majority-element.rb
+++ arrays/majority-element.rb
@@ -19,8 +19,6 @@ def get_majority_element(a, left, right)
   -1
 end
 
-if __FILE__ == $0
-  n, *a = STDIN.read.split.map(&:to_i)
-  answer = get_majority_element(a, 0, n - 1) != -1
-  puts "#{answer ? 1 : 0}"
-end
+n, *a = STDIN.read.split.map(&:to_i)
+answer = get_majority_element(a, 0, n - 1) != -1
+puts "#{answer ? 1 : 0}"
